ae630358e98af8819c2ae226722b5b917db87ad5
GeneralD <yumejustice@gmail.com>
2024-01-24 13:40:02 +0900

    refactor: fix type suggestion

--- libraries/const.ts
+++ libraries/const.ts
@@ -1,5 +1,6 @@
+import { Contract } from 'ethers'
 import { __SYMBOL__ } from '../typechain-types/contracts/__SYMBOL__'
 import { ethers } from 'hardhat'
 
 export const latest__SYMBOL__Factory = ethers.getContractFactory("__SYMBOL__")
-export type Latest__SYMBOL__ = __SYMBOL__
+export type Latest__SYMBOL__ = __SYMBOL__ & Contract
